24c0bc4eeca553e54a6130275be7c262933bc9b2
AkiThung <28469@ma-web.nl>
2020-10-07 12:20:01 +0200

    onderzoek textbased applicatie

--- ditbenik.py
+++ ditbenik.py
@@ -9,34 +9,37 @@ while a == True:
     time.sleep (1)
 
     username = input("Mijn naam is:")
-    print("Hallo " + username, "hier is je datum ")
+    print("Hallo " + username, "welkom in TAFARIA")
 
     time.sleep (1)
 
-    import datetime
-
-    x = datetime.datetime.now()
-
-    print("vandaag is het " + x.strftime("%d ") + x.strftime ("%B"))
     while True:
      time.sleep( 2 )
 
-     antwoord1 = input("ik kom uit \n A Amsterdam \n B Haarlem \n C Utrecht \n antwoord: ")
+     antwoord1 = input("Je komt aan  in TAFARIA. De eerste vraag in deze wereld is welk ras wil je worden, aangezien alles in deze wereld kan \n A Dwerg \n B Elf \n C Minataurus \n antwoord: ")
      if ( antwoord1 == "A"):
-        print("goed")
+        print("eyy Kleintje")
      elif ( antwoord1 == "B"):
-        print("fout")
+        print("Welkom onsterfelijke")
      elif ( antwoord1 == "C"):
-        print("fout")
+        print("Nice cut G, like them horns")
      time.sleep( 2 )
         
-     antwoord2 = input("Hoe oud ben ik? \n A 18 \n B 16  \n C 17 \n antwoord: ")
+     antwoord2 = input("Wat is je class \n A Archer \n B Warrior  \n C Mage \n antwoord: ")
      if ( antwoord2 == "A"):
-         print("fout")
+         print("Scherpschutter ;)")
      elif ( antwoord2 == "B"):
-        print("fout")
+        print("Strong Warrior u are the base of our existence as a clan")
      elif ( antwoord2 == "C"):
-        print("goed")
+        print("Gekke Magie, Als Mage ga je naar de citadel genaamd SEKARA om te leren je krachten te gebruiken")
+        antwoord4 = input("Wat is je  preference binnen de Magie?\n A Vuur \n B Water \n C Bloodmagic \n antwoord:")
+        if ( antwoord4 == "A"):
+            print("FIREEE")
+        elif ( antwoord4 == "B"):
+            print("can u make a tsunami??")
+        elif ( antwoord4 == "C"):
+            print("the powers of the evil")
+         break  
      time.sleep( 2 )
      antwoord3 = input("Wanneer ben ik jarig \n A 24 mei \n B 25 mei \n C 26 mei \n antwoord:")
      if ( antwoord3 == "A"):
